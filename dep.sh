25c6d038ec62bcb05515b2fad44147310ff1b45d
gogeof <miancai.li@gmail.com>
2023-09-18 19:06:40 +0800

    add dep.sh

---
+++ dep.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+install_docker(){
+	sudo yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+
+
+	sudo yum install -y yum-utils
+
+	sudo yum-config-manager \
+    		--add-repo \
+    		http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+
+
+	sudo yum -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+}
+
+main(){
+	install_docker
+}
+
+main $@
